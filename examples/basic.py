1d8409b07d9571a0823ac3c1c4a967ed126341e8
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2021-12-13 17:14:59 +0700

    Update basic.py

--- examples/basic.py
+++ examples/basic.py
@@ -6,12 +6,8 @@ rpc = DiscordRPC.RPC.Set_ID(app_id=app_id)
 
 while True:
     rpc.set_activity(
-      state="up to you", 
-      details="up to you", 
-      timestamp=rpc.timestamp(), 
-      large_text="Competitive", 
-      small_text="Radiant", 
-      large_image="Valorant", 
-      small_image="Radiant_logo"
+      state="Rank : Radiant", 
+      details="Competitive", 
+      timestamp=rpc.timestamp()
     )
     time.sleep(600) # to update the PC, recommended: every 10 minutes or 600 seconds
