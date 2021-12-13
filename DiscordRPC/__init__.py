78d98c3a823071090fe2d9ff1d0ce56d673fadff
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2021-12-13 12:55:33 +0700

    Create __init__.py

---
+++ DiscordRPC/__init__.py
@@ -0,0 +1,7 @@
+from .rpc import *
+
+__title__ = "Discord-RPC"
+__version__ = "1.0"
+__authors__ = ["LyQuid", "EterNomm"]
+__license__ = "Apache License 2.0"
+__copyright__ = "Copyright 2021 EterNomm"
