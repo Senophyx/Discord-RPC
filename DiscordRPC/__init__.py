77a6cbc3bf7100666e06d2b4ffd3e601ddf780a1
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2021-12-13 14:33:51 +0700

    Update version

--- DiscordRPC/__init__.py
+++ DiscordRPC/__init__.py
@@ -1,7 +1,7 @@
 from .presence import *
 
 __title__ = "Discord-RPC"
-__version__ = "1.0"
+__version__ = "1.2"
 __authors__ = ["LyQuid", "EterNomm"]
 __license__ = "Apache License 2.0"
 __copyright__ = "Copyright 2021 EterNomm"
