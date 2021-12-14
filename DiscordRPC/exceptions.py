9d4925d3fd7a13b9bd67f9762635210fa190ad97
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2021-12-14 10:31:16 +0700

    Create exceptions.py

---
+++ DiscordRPC/exceptions.py
@@ -0,0 +1,29 @@
+class RPCException(Exception):
+    def __init__(self, message: str= None):
+        if message is None:
+            message = 'An error has occurred within DiscordRPC'
+        super().__init__(message)
+
+class Error(RPCException):
+    def __init__(self, message:str):
+        super().__init__(message)
+
+class DiscordNotOpened(RPCException):
+    def __init__(self):
+        super().__init__("Error, could not find Discord. is Discord running?")
+
+class ActivityError(RPCException):
+    def __init__(self):
+        super().__init__("An error has occurred in activity payload, do you have set your activity correctly")
+
+class InvalidURL(RPCException):
+    def __init__(self):
+        super().__init__("Invalid URL. Must include: http:// or https://")
+
+class InvalidID(RPCException):
+    def __init__(self):
+        super().__init__("Invalid ID, is the ID correct?")
+
+class ButtonError(RPCException):
+    def __init__(self, message: str = None):
+        super().__init__(message=message)
