158ef9f8f320a6ebf429bb5a00a4ddd07a7607f1
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2021-12-14 10:57:13 +0700

    Update rpc-with-button.py

--- examples/rpc-with-button.py
+++ examples/rpc-with-button.py
@@ -1,9 +1,14 @@
 import DiscordRPC
 import time 
 
-rpc = DiscordRPC.RPC.Set_ID(app_id="919914790556672031") # Application ID must string
+rpc = DiscordRPC.RPC.Set_ID(app_id=919914790556672031) # Application ID must string
 
-button = DiscordRPC.button(button_one_label="Repository", button_one_url="https://github.com/LyQuid12/discord-rpc", button_two_label="Discord Server", button_two_url="https://discord.gg/qpT2AeYZRN")
+button = DiscordRPC.button(
+  button_one_label="Repository",
+  button_one_url="https://github.com/LyQuid12/discord-rpc",
+  button_two_label="Discord Server",
+  button_two_url="https://discord.gg/qpT2AeYZRN"
+  )
 
 while True:
     rpc.set_activity(
@@ -12,4 +17,5 @@ while True:
       timestamp=rpc.timestamp(),
       buttons=button
     )
+    print(rpc.output())
     time.sleep(600) # to update the PC, recommended: every 10 minutes or 600 seconds
