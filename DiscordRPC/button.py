49cf79aba7abedc3d3d41def6c13250e5bd3234b
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2021-12-14 10:44:15 +0700

    Error message now using exceptions file

--- DiscordRPC/button.py
+++ DiscordRPC/button.py
@@ -1,3 +1,5 @@
+from .exceptions import *
+
 valid_url = ["https://", "http://"]
 
 def button_one(label:str, url:str):
@@ -5,16 +7,30 @@ def button_one(label:str, url:str):
         payloads = {"label": label, "url": url}
         return payloads
     else:
-        raise TypeError("Invalid URL. Must include: http:// or https://")
+        raise InvalidURL
 
 def button_two(label:str, url:str):
     if any(v in url for v in valid_url):
         payloads = {"label": label, "url": url}
         return payloads
     else:
-        raise TypeError("Invalid URL. Must include: http:// or https://")
+        raise InvalidURL
 
-def button(button_one_label:str, button_two_label:str, button_one_url:str, button_two_url:str):
+def button(
+    button_one_label:str=None,
+    button_two_label:str=None,
+    button_one_url:str=None,
+    button_two_url:str=None):
+    
+    if button_one_label == None:
+        raise ButtonError('"button_one_label" cannot None')
+    if button_one_url == None:
+        raise ButtonError('"button_one_url" cannot None')
+    if button_two_label == None:
+        raise ButtonError('"button_two_label" cannot None')
+    if button_two_url == None:
+        raise ButtonError('"button_two_url" cannot None')
+    
     btn_one = button_one(label=button_one_label, url=button_one_url)
     btn_two = button_two(label=button_two_label, url=button_two_url)
     payloads = [btn_one, btn_two]
