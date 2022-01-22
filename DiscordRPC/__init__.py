5f9dac2400c71bc5b08e7042e81f2236b946903d
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-01-22 22:52:44 +0700

    Update __init__.py

--- DiscordRPC/__init__.py
+++ DiscordRPC/__init__.py
@@ -1,9 +1,10 @@
 from .presence import *
 from .button import button
 from .exceptions import *
+from .get_current_app import GCAR
 
 __title__ = "Discord-RPC"
-__version__ = "2.0"
-__authors__ = ["LyQuid", "EterNomm"]
+__version__ = "3.0"
+__authors__ = "LyQuid"
 __license__ = "Apache License 2.0"
-__copyright__ = "Copyright 2021 EterNomm"
+__copyright__ = "Copyright 2021-present LyQuid"
