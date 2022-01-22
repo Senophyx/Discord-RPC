58224a3329bd57edb48fb14ad0c15c566a010bed
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-01-22 22:48:40 +0700

    Create GCAR method

--- DiscordRPC/get_current_app.py
+++ DiscordRPC/get_current_app.py
@@ -1,1 +1,19 @@
+import psutil
+import win32process
+import win32gui
 
+def GCAR():
+    r"""
+    GCAR : Get Current Application Running. a method to get your current application you open.
+    ---
+    Parameter :
+    Nothing just add () after GCAR, like `GCAR()`
+    """
+    pid = win32process.GetWindowThreadProcessId(win32gui.GetForegroundWindow())
+    app = psutil.Process(pid[-1]).name()
+    app = app.replace(".exe", "")
+    app = app.capitalize()
+    if app == "Code": # Because if you open visual studio code, only "Code" will appears, so I just changed it ;>
+        app = "Visual Studio Code"
+    
+    return app
