09470b4146a9cb8f7bec24bd76ade9b37da51276
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-05-31 14:44:24 +0700

    update version to `3.5`

--- DiscordRPC/__init__.py
+++ DiscordRPC/__init__.py
@@ -1,10 +1,10 @@
 from .presence import *
 from .button import button
 from .exceptions import *
-# from .get_current_app import GCAR (Disabling due to a bug)
+#from .get_current_app import GCAR (Disabling due to a bug)
 
 __title__ = "Discord-RPC"
-__version__ = "3.0"
+__version__ = "3.5"
 __authors__ = "LyQuid"
 __license__ = "Apache License 2.0"
 __copyright__ = "Copyright 2021-present LyQuid"
