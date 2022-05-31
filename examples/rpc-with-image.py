d5a63b237d4bba37f055a290dae217182392ecb5
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-05-31 14:45:31 +0700

    removing button

--- examples/rpc-with-image.py
+++ examples/rpc-with-image.py
@@ -5,21 +5,13 @@ rpc = DiscordRPC.RPC.Set_ID(app_id=123456789101112)
 # Upload your image(s) here:
 # https://discordapp.com/developers/applications/<APP ID>/rich-presence/assets
 
-button = DiscordRPC.button(
-  button_one_label="Repository",
-  button_one_url="https://github.com/LyQuid12/discord-rpc",
-  button_two_label="Discord Server",
-  button_two_url="https://discord.gg/qpT2AeYZRN"
-  )
-
 rpc.set_activity(
       state="pip install discord-rpc",
       details="Discord-RPC by LyQuid",
       large_image="eternomm_logo", # Make sure you are using the same name that you used when uploading the image
       large_text="EterNomm",
       small_image="github", # Make sure you are using the same name that you used when uploading the image
-      small_text="Github",
-      buttons=button
+      small_text="Github"
     )
 
 
