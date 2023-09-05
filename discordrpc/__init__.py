4757b3a3a352b1433a36d7a4efc39a3333523aab
LyQuid <83483788+LyQuid12@users.noreply.github.com>
2023-09-05 11:31:35 +0700

    Update and changing folder to `discordrpc`

---
+++ discordrpc/__init__.py
@@ -0,0 +1,10 @@
+from .presence import RPC
+from .button import button
+from .exceptions import *
+#from .get_current_app import GCAR (Disabled due to a bug)
+
+__title__ = "Discord RPC"
+__version__ = "4.0"
+__authors__ = "LyQuid"
+__license__ = "MIT License"
+__copyright__ = "Copyright 2021-present LyQuid"
