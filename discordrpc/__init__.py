3fc8762fabd10fb6e87de36682e7a309983eb0fc
LyQuid <83483788+LyQuid12@users.noreply.github.com>
2023-09-05 11:54:35 +0700

    Removing `GCAR`

--- discordrpc/__init__.py
+++ discordrpc/__init__.py
@@ -1,7 +1,6 @@
 from .presence import RPC
 from .button import button
 from .exceptions import *
-#from .get_current_app import GCAR (Disabled due to a bug)
 
 __title__ = "Discord RPC"
 __version__ = "4.0"
