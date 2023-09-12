c5e31828760a96292106af2f807dde1b8cc47e8d
LyQuid <83483788+LyQuid12@users.noreply.github.com>
2023-09-12 08:59:55 +0700

    Update `__version__` to 4.0

--- discordrpc/__init__.py
+++ discordrpc/__init__.py
@@ -3,7 +3,7 @@ from .button import button
 from .exceptions import *
 
 __title__ = "Discord RPC"
-__version__ = "4.0b1"
+__version__ = "4.0"
 __authors__ = "LyQuid"
 __license__ = "MIT License"
 __copyright__ = "Copyright 2021-present LyQuid"
