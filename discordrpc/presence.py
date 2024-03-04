c006f90dc0ad6b2c9c40248610344c3204507848
senophyx <lyquidpersonal@gmail.com>
2024-03-04 09:36:40 +0700

    Big updates. See changelogs for more details.

--- discordrpc/presence.py
+++ discordrpc/presence.py
@@ -1,252 +1,251 @@
-import json
+import sys
 import os
 import socket
-import sys
+import json
 import struct
+import uuid
+import re
 import time
 from .exceptions import *
+from .utils import remove_none
 
 
 OP_HANDSHAKE = 0
 OP_FRAME = 1
 OP_CLOSE = 2
-OP_PING = 3
-OP_PONG = 4
+
 
 
 class RPC:
-    def __init__(self, client_id):
-        self.client_id = client_id
-        self._connect()
-        self._do_handshake()
-        self._output = None
-        self.show_output = False
-
-
-    @classmethod
-    def set_id(self, app_id):
+    def __init__(self, app_id:int):
         app_id = str(app_id)
-        platform = sys.platform
-        if platform == 'win32':
-            return DiscordWindows(app_id)
+        self.app_id = app_id
+
+        self.debug = True
+        # Soon :
+        # self.show_output = False
+        # self.is_connected = False
+        # self.is_running = False
+
+        if sys.platform == "win32":
+            self.ipc = WindowsPipe(app_id, self.debug)
+            self.ipc.handshake()
+
         else:
-            return DiscordUnix(app_id)
-        
+            self.ipc = UnixPipe(app_id, self.debug)
+            self.ipc.handshake()
+
+
+
+    timestamp = time.time()
     
-    def _connect(self):
-        pass
 
-
-    def _do_handshake(self):
-        op, data = self.send_recv({'v': 1, 'client_id': self.client_id}, op=OP_HANDSHAKE)
-        if op == OP_FRAME and data['cmd'] == 'DISPATCH' and data['evt'] == 'READY':
-            return
-        else:
-            if op == OP_CLOSE:
-                self.close()
-                if data['code'] == 4000:
-                    raise InvalidID()
-            raise RPCException(f"Unknown error : {data}")
-        
-
-    def _write(self, data:bytes):
-        pass
-
-    def _recv(self, size:int) -> bytes:
-        pass
-
-    def _recv_header(self):
-        header = self._recv_exact(8)
-        return struct.unpack("<II", header)
-    
-    def _recv_exact(self, size_remaining) -> bytes:
-        buf = b""
-        while size_remaining:
-            chunk = self._recv(size_remaining)
-            buf += chunk
-            size_remaining -= len(chunk)
-        return buf
-    
-    def close(self):
-        try:
-            self.send({}, op=OP_CLOSE)
-        finally:
-            self._close()
-
-    
-    def _close(self):
-        pass
-
-    def __enter__(self):
-        return self
-    
-    def __exit__(self, *_):
-        self.close()
-
-    def send_recv(self, data, op=OP_FRAME):
-        self.send(data, op)
-        return self.recv()
-    
-    def send(self, data, op=OP_FRAME):
-        data = json.dumps(data, separators=(',', ':')).encode('UTF-8')
-        header = struct.pack("<II", op, len(data))
-        self._write(header)
-        self._write(data)
-
-    def recv(self):
-        op, length = self._recv_header()
-        payload = self._recv_exact(length)
-        data = json.loads(payload.decode('UTF-8'))
-        return op, data
-    
-    timestamp = time.mktime(time.localtime())
-    
     def set_activity(
             self,
-            state:str=None,
-            details:str=None,
-            timestamp=None,
-            small_text:str='null',
-            large_text:str='null',
-            small_image:str='null',
-            large_image:str='null',
-            buttons=None
-    ):
-        
-        if len(large_text) <= 1:
-            raise Error('"large text" must be at least above 1 characters')
-        
-        if len(small_text) <= 1:
-            raise Error('"small text" must be at least above 1 characters')
-        
+            state: str=None, details:str=None,
+            start: int=None, end: int = None,
+            large_image:str=None, large_text:str=None,
+            small_image:str=None, small_text:str=None,
+            party_id:str=None, party_size: list=None,
+            join_secret:str=None, spectate_secret:str=None,
+            match_secret:str=None, buttons=None
+        ):
+
         act = {
             "state": state,
             "details": details,
             "timestamps": {
-                "start": timestamp
+                "start": start,
+                "end": end
             },
             "assets": {
-                "small_text": small_text,
+                "large_image": large_image,
                 "large_text": large_text,
-                "small_image": str(small_image),
-                "large_image": str(large_image)
+                "small_image": small_image,
+                "small_text": small_text
             },
-            "buttons": buttons,
+            "party": {
+                "id": party_id,
+                "size": party_size
+            },
+            "secrets": {
+                "join": join_secret,
+                "spectate": spectate_secret,
+                "match": match_secret
+            },
+            "buttons": buttons
         }
 
-        if state == None or state == '' or state == 'null':
-            act.pop('state', None)
-        if details == None or details == '' or details == 'null':
-            act.pop('details', None)
-        if small_text == None or small_text == '' or small_text == 'null':
-            act['assets'].pop('small_text', None)
-        if large_text == None or large_text == '' or large_text == 'null':
-            act['assets'].pop('large_text', None)
-        if timestamp == None:
-            act.pop('timestamps', None)
-        if buttons == None:
-            act.pop('buttons', None)
 
-
-        data = {
+        payload = {
             'cmd': 'SET_ACTIVITY',
             'args': {
                 'pid': os.getpid(),
-                'activity': act
+                'activity': remove_none(act)
             },
-            'nonce': '{:.20f}'.format(time.time())
+            'nonce': str(uuid.uuid4())
         }
 
-        self.send(data)
+        self.ipc._send(payload, OP_FRAME)
 
-        op, length = self._recv_header()
-        payload = self._recv_exact(length)
-        output = json.loads(payload.decode('UTF-8'))
-        if output['evt'] == 'ERROR':
-            raise ActivityError
+    
+    def disconnect(self):
+        self.ipc.disconnect()
+
+
+
+
+class WindowsPipe:
+    def __init__(self, app_id, debug):
+        self.app_id = app_id
+        self.debug = debug
+
+        base_path = R'\\?\pipe\discord-ipc-{}'
+        
+        for i in range(10):
+            path = base_path.format(i)
+
+            try:
+                self.socket = open(path, "w+b")
+            except OSError as e:
+                raise Error("Failed to open {!r}: {}".format(path, e))
+            else:
+                break
+
+        else:
+            raise DiscordNotOpened()
+        
+        if self.debug == True:
+            print(f"Connected to {path}")
+
+
+    def _recv(self):
+        enc_header = b''
+        header_size = 8
+
+        while header_size:
+            enc_header += self.socket.read(header_size)
+            header_size -= len(enc_header)
+
+        dec_header = struct.unpack("<ii", enc_header)
+        enc_data = b''
+        remain_packet_size = int(dec_header[1])
+
+        while remain_packet_size:
+            enc_data += self.socket.read(remain_packet_size)
+            remain_packet_size -= len(enc_data)
+        
+        
+        output = json.loads(enc_data.decode('UTF-8'))
+
+        if self.debug == True:
+            print(output)
+
+        return output
+    
+
+    def _send(self, payload, op=OP_FRAME):
+        if self.debug == True:
+            print(payload)
+
+        payload = json.dumps(payload).encode('UTF-8')
+        payload = struct.pack('<ii', op, len(payload)) + payload
+
+        self.socket.write(payload)
+        self.socket.flush()
+
+
+    def handshake(self):
+        self._send({'v': 1, 'client_id': self.app_id}, op=OP_HANDSHAKE)
+        data = self._recv()
+
+        if data['cmd'] == 'DISPATCH' and data['evt'] == 'READY':
+            print(f"Connected to {data['data']['user']['global_name']} ({data['data']['user']['username']}) | User ID : {data['data']['user']['id']}")
+            return True
         
         else:
-            if self.show_output==True:print(f"Successfully set RPC for {self.client_id}")
+            if data['code'] == 4000:
+                raise InvalidID
+            
 
-        return op, output
-
-    
-
-    def run(self):
-        try:
-            while True:
-                time.sleep(1)
-        except KeyboardInterrupt:
-            self.close()
-
-
-
-class DiscordWindows(RPC):
-    _pipe_path = R'\\?\pipe\discord-ipc-{}'
-
-    def _connect(self):
-        for i in range(10):
-            path = self._pipe_path.format(i)
-
-            try:
-                self._open = open(path, "w+b")
-            except OSError as e:
-                raise Error("Failed to open {!r}: {}".format(path, e))
-            else:
-                break
-
-        else:
-            raise DiscordNotOpened
-
-        self.path = path
-
-    def _write(self, data:bytes):
-        self._open.write(data)
-        self._open.flush()
-
-    def _recv(self, size:int) -> bytes:
-        return self._open.read(size)
-    
-    def _close(self):
-        self._open.close()
+    def disconnect(self):
+        self._send({}, OP_CLOSE)
+        
+        self.socket.close()
+        self.socket = None
+        print("Closing")
+        exit()
 
 
 
-class DiscordUnix(RPC):
-    def _connect(self):
-        self._socket = socket.socket(socket.AF_UNIX)
-        pipe_path = self._get_pipe_path()
+class UnixPipe:
+    def __init__(self, app_id, debug):
+        self.app_id = app_id
+        self.debug = debug
+
+        self.socket = socket.socket(socket.AF_UNIX)
+
+        base_path = path = os.environ.get('XDG_RUNTIME_DIR') or os.environ.get('TMPDIR') or os.environ.get('TMP') or os.environ.get('TEMP') or '/tmp'
+        base_path = re.sub(r'\/$', '', path) + '/discord-ipc-{0}'
 
         for i in range(10):
-            path = pipe_path.format(i)
-            if not os.path.exists(path):
-                continue
+            path = base_path.format(i)
+
             try:
-                self._socket.connect(path)
-            except OSError as e:
-                raise Error("Failed to open {!r}: {}".format(path, e))
-            else:
+                self.socket.connect(path)
                 break
+            except FileNotFoundError:
+                pass
+
         else:
-            raise DiscordNotOpened
+            raise DiscordNotOpened()
+        
+        if self.debug == True:
+            print(f"Connected to {path}")
 
 
-    def _get_pipe_path(self):
-        env_keys = ('XDG_RUNTIME_DIR', 'TMPDIR', 'TMP', 'TEMP')
-        for env_key in env_keys:
-            dir_path = os.environ.get(env_key)
-            if dir_path:
-                break
-        else:
-            dir_path = '/tmp'
-        return os.path.join(dir_path, 'discord-ipc-{}')
+    def _recv(self):
+        recv_data = self.socket.recv(1024)
+        enc_header = recv_data[:8]
+        dec_header = struct.unpack("<ii", enc_header)
+        enc_data = recv_data[8:]
+
+        output = json.loads(enc_data.decode('UTF-8'))
+    
+        if self.debug == True:
+            print(output)
+
+        return output
     
 
-    def _write(self, data:bytes):
-        self._socket.sendall(data)
+    def _send(self, payload, op=OP_FRAME):
+        if self.debug == True:
+            print(payload)
 
-    def _recv(self, size:int) -> bytes:
-        return self._socket.recv(size)
+        payload = json.dumps(payload).encode('UTF-8')
+        payload = struct.pack('<ii', op, len(payload)) + payload
+
+        self.socket.send(payload)
+
     
-    def _close(self):
-        self._socket.close()
+    def handshake(self):
+        self._send({'v': 1, 'client_id': self.app_id}, op=OP_HANDSHAKE)
+        data = self._recv()
+
+        if data['cmd'] == 'DISPATCH' and data['evt'] == 'READY':
+            print(f"Connected to {data['data']['user']['global_name']} ({data['data']['user']['username']}) | User ID : {data['data']['user']['id']}")
+            return True
+        
+        else:
+            if data['code'] == 4000:
+                raise InvalidID()
+            
+
+    def disconnect(self):
+        self._send({}, OP_CLOSE)
+
+        self.socket.shutdown(socket.SHUT_RDWR)
+        self.socket.close()
+        self.socket = None
+        print("Closing")
+        exit()
