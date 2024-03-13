433ce90b6b795d478392da854b04ea6c84f1a033
senophyx <lyquidpersonal@gmail.com>
2024-03-13 15:31:27 +0700

    Removing `button_one` and `button_two` functions and changed to `_payload`. Also update `button` to `Button`

--- discordrpc/button.py
+++ discordrpc/button.py
@@ -2,25 +2,19 @@ from .exceptions import *
 
 valid_url = ["https://", "http://"]
 
-def button_one(label:str, url:str):
+def _payload(label:str, url:str):
     if any(v in url for v in valid_url):
         payloads = {"label": label, "url": url}
         return payloads
     else:
         raise InvalidURL
+    
 
-def button_two(label:str, url:str):
-    if any(v in url for v in valid_url):
-        payloads = {"label": label, "url": url}
-        return payloads
-    else:
-        raise InvalidURL
-
-def button(
-    button_one_label:str=None,
-    button_two_label:str=None,
-    button_one_url:str=None,
-    button_two_url:str=None):
+def Button(
+    button_one_label:str,
+    button_one_url:str,
+    button_two_label:str,
+    button_two_url:str):
     
     if button_one_label == None:
         raise ButtonError('"button_one_label" cannot None')
@@ -31,8 +25,8 @@ def button(
     if button_two_url == None:
         raise ButtonError('"button_two_url" cannot None')
     
-    btn_one = button_one(label=button_one_label, url=button_one_url)
-    btn_two = button_two(label=button_two_label, url=button_two_url)
+    btn_one = _payload(label=button_one_label, url=button_one_url)
+    btn_two = _payload(label=button_two_label, url=button_two_url)
     payloads = [btn_one, btn_two]
 
     return payloads
