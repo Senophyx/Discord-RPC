52063fd5c3c3703ed4da79aac754c3cb5554f925
ByFox <byfox213@gmail.com>
2024-08-30 12:32:01 +0500

    ButtonException
    Added an error if there are more than two buttons or none at all

--- discordrpc/button.py
+++ discordrpc/button.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-from discordrpc import InvalidURL
+from discordrpc import InvalidURL, ButtonError
 
 
 @dataclass
@@ -17,9 +17,15 @@ class Button:
 class Buttons:
     def __init__(self, *args):
         if len(args) == 1 and isinstance(args[0], list):
-            self.payload = [i.get_payload() for i in args[0] if isinstance(i, Button)]
+            payload = [i.get_payload() for i in args[0] if isinstance(i, Button)]
         else:
-            self.payload = [i.get_payload() for i in args if isinstance(i, Button)]
+            payload = [i.get_payload() for i in args if isinstance(i, Button)]
+
+        count_buttons = len(payload)
+        if count_buttons == 0 or count_buttons > 2:
+            raise ButtonError("The number of buttons exceeds two or there are none at all")
+        
+        self.payload = payload
 
     def get_payloads(self):
         return self.payload
