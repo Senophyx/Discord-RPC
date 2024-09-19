8e8593641294af01291e856b8ab425c2b038a7c0
Senophyx <lyquidpersonal@gmail.com>
2024-09-19 08:42:52 +0700

    Update version to `5.1`

--- discordrpc/__init__.py
+++ discordrpc/__init__.py
@@ -4,7 +4,7 @@ from .exceptions import *
 from .utils import timestamp, date_to_timestamp
 
 __title__ = "Discord RPC"
-__version__ = "5.1b1"
+__version__ = "5.1"
 __authors__ = "Senophyx"
 __license__ = "MIT License"
 __copyright__ = "Copyright 2021-2024 Senophyx"
