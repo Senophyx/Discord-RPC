1a1a653148199ced147392c66502670f1c771612
Senophyx <83483788+Senophyx@users.noreply.github.com>
2025-07-26 14:51:19 +0700

    Revert changes types.py

--- discordrpc/types.py
+++ discordrpc/types.py
@@ -2,12 +2,11 @@ from enum import Enum
 
 
 # https://discord.com/developers/docs/events/gateway-events#activity-object-activity-types
-# Activity Type 1 and 4 currently disabled. See https://github.com/Senophyx/Discord-RPC/issues/28#issuecomment-2301287350
 
 class Activity(Enum):
     Playing = 0
-    #Streaming = 1
+    Streaming = 1
     Listening = 2
     Watching = 3
-    #Custom = 4
+    Custom = 4
     Competing = 5
