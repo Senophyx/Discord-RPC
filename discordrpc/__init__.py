4734d0ace4b8f1dbbe6eb69a02271421a2cd6b95
Senophyx <lyquidpersonal@gmail.com>
2025-10-08 15:20:58 +0700

    update version to `5.5b1`

--- discordrpc/__init__.py
+++ discordrpc/__init__.py
@@ -1,12 +1,11 @@
 from .presence import RPC
 from .button import Button
-from .progressbar import Progressbar
 from .exceptions import *
 from .types import *
-from .utils import timestamp, date_to_timestamp, use_local_time
+from .utils import *
 
 __title__ = "Discord RPC"
-__version__ = "5.2b1"
+__version__ = "5.5b1"
 __authors__ = "Senophyx"
 __license__ = "MIT License"
 __copyright__ = "Copyright 2021-2025 Senophyx"
