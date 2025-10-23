0ffaca5bbf941a1435defac789c966a8d7d3ad5c
Senophyx <lyquidpersonal@gmail.com>
2025-10-23 11:19:42 +0700

    migrating to PEP 621 (pyproject.toml)

--- discordrpc/__init__.py
+++ discordrpc/__init__.py
@@ -1,3 +1,4 @@
+from importlib.metadata import version as _pkg_ver
 from .presence import RPC
 from .button import Button
 from .exceptions import *
@@ -5,7 +6,7 @@ from .types import *
 from .utils import *
 
 __title__ = "Discord RPC"
-__version__ = "5.5b1"
+__version__ = _pkg_ver('discord-rpc')
 __authors__ = "Senophyx"
 __license__ = "MIT License"
 __copyright__ = "Copyright 2021-2025 Senophyx"
--- setup.py
+++ setup.py
@@ -1,60 +1,7 @@
-from setuptools import setup, find_packages
-import re
+"""
+setup.py configuration has been migrated to pyproject.toml
+latest legacy setup.py config : commit #829e81c
+"""
 
-with open('README.md', encoding='utf-8') as f:
-    long_description = f.read()
-
-version = ''
-with open('discordrpc/__init__.py', encoding='utf-8') as f:
-    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
-
-if not version:
-    raise RuntimeError('Version is not set')
-
-
-# Not needed because there are currently no required packages in requirements.txt
-
-# def read_requirements():
-#     with open('requirements.txt', 'r') as req:
-#         content = req.read()
-#         requirements = content.split('\n')
-
-#     return requirements
-
-
-
-setup(
-    name='discord-rpc',
-    version=version,
-    author='Senophyx',
-    author_email='contact@senophyx.id',
-    description='An Python wrapper for Discord RPC',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    license='MIT License',
-    url='https://github.com/Senophyx/discord-rpc',
-    project_urls={
-        "Discord": "https://discord.gg/qpT2AeYZRN",
-        "Documentation" : "https://github.com/Senophyx/Discord-RPC/blob/main/DOCS.md",
-        "Issue tracker": "https://github.com/Senophyx/discord-rpc/issues"
-    },
-    packages=find_packages(),
-    include_package_data=True,
-    #install_requires=read_requirements(),
-    keywords=["Discord", "rpc", "discord rpc"],
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Intended Audience :: End Users/Desktop',
-        'Intended Audience :: Other Audience',
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: MacOS',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python'
-    ]
-)
+from setuptools import setup
+setup()
