167477c04e8e6c5e61692d581ca1e9ad15294912
Senophyx <lyquidpersonal@gmail.com>
2026-06-14 18:48:53 +0700

    chore(release): bump version to 5.6

--- discordrpc/__init__.py
+++ discordrpc/__init__.py
@@ -9,7 +9,7 @@ __title__ = "Discord RPC"
 try:
     __version__ = _pkg_ver('discord-rpc')
 except PackageNotFoundError:
-    __version__ = "unknown"
+    __version__ = "5.6"
 __authors__ = "Senophyx"
 __license__ = "MIT License"
 __copyright__ = "Copyright 2021-2025 Senophyx"
